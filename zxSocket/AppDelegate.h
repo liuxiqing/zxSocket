458069611d54153c19e6f8d92c4115fecc36b823
张 玺 <zhangxi@localhost.(none)>
2012-03-24 16:27:39 +0800

    Initial Commit

---
+++ zxSocket/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  zxSocket
+//
+//  Created by 张 玺 on 12-3-24.
+//  Copyright (c) 2012年 张玺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ zxSocket/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  zxSocket
+//
+//  Created by 张 玺 on 12-3-24.
+//  Copyright (c) 2012年 张玺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
